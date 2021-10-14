131c44daf8affc6ad55b2a73e1d77338eed8d64e
mcleodheads <vamaklakov@mmtr.ru>
2021-10-14 18:13:23 +0300

    Home page is ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import * as React from 'react';
 import {useHistory, Link} from "react-router-dom";
 import st from '../assets/styles/header.module.css'
-import {HOME_ROUTE} from "../utils/paths";
+import {FORM_ROUTE, HOME_ROUTE} from "../utils/paths";
 
 const Header = () => {
     const history = useHistory() // history.location.pathname
-
+    const [isVisible, setVisible] = React.useState(true)
     return (
         <div className={st.wrapper}>
             <div className={st.actions}>
                 <div className={st.logo}></div>
                 {
-                    history.location.pathname !== '/' && history.location.pathname !== '/form' ? (
+                    isVisible ? (
                         <Link to={HOME_ROUTE}>
                             <button className={st.button}>
-                            <span className={st.btnInner}>
-                                Завершить
-                            </span>
+                        <span className={st.btnInner}>
+                            Завершить
+                        </span>
                             </button>
                         </Link>
                     ) : null
--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import st from '../assets/styles/homeScreen.module.css'
+import {GAME_ROUTE} from "../utils/paths";
+import {useHistory} from "react-router-dom";
 
 const HomeScreen = () => {
+    const history = useHistory()
+
     return (
         <div className={st.wrapper}>
             <div className={st.container}>
                 <div className={st.content}>
                     <div className={st.header}>
-
+                        Тренажер “Треугольники”
                     </div>
                     <div className={st.body}>
-
+                        В отличие от тестов, здесь вы можете применить свои знания на практике.
+                        Этот<br/> тренажер эмулирует популярную среду для тестирования.<br/>
+                        Можете попробовать свои силы, найти все баги и все кейсы, которые мы туда<br/>
+                        запрятали.<br/>
+                        Успехов! :)
                     </div>
-                    <div className={st.action}>
-                        <button></button>
-                    </div>
+                    <button className={st.action} onClick={() => history.push(GAME_ROUTE)}>
+                        <span className={st.innerBtn}>Запустить тренажер</span>
+                    </button>
                 </div>
                 <div className={st.triangle}></div>
             </div>
